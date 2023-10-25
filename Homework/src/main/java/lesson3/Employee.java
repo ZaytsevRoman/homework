a2413fcc4bc7368ac34834b7fb650c62ee558e96
Roman <romaz@bk.ru>
2023-10-25 11:30:23 +0600

    lesson 3

---
+++ Homework/src/main/java/lesson3/Employee.java
@@ -0,0 +1,31 @@
+package lesson3;
+
+public class Employee {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.println("Сотрудник{" +
+                "ФИО='" + fullName + '\'' +
+                ", Должность='" + position + '\'' +
+                ", Email='" + email + '\'' +
+                ", Номер телефона=" + phoneNumber +
+                ", Зарплата=" + salary +
+                ", Возраст=" + age +
+                '}');
+    }
+}
---
+++ Homework/src/main/java/lesson3/Park.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+public class Park {
+
+    private String parkName;
+    private String address;
+
+
+    public class Attractions {
+
+        private String attractionName;
+        private String openingHours;
+        private int price;
+    }
+}
